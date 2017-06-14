b58e14b590984a312a51acf85f8777050829466a
Payam Gerami <payam.gerami@hotmail.com>
2017-06-14 14:16:30 -0400

    Add api project structure

---
+++ src/Eventful.Api/Eventful.Api/Controllers/VersionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eventful.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class VersionController
+    {
+        // GET api/version
+        [HttpGet]
+        public string Get()
+        {
+            return "V1";
+        }
+    }
+}
